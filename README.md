dcfa653db52275f9884618af49476158438003a0
Philipp <philippfromme@outlook.com>
2022-07-15 19:19:57 +0200

    feat: add second variant

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-# Linting Playground 🚧🏗️👷🚧
+# Linting Playground
+
+## Prototypes
+
+1. [Web Modeler style](https://philippfrom.me/linting-playground/?variant=2)
+2. [Web Modeler style with opacity](https://philippfrom.me/linting-playground/?variant=2&opacity=1)
+3. [Web Modeler style with number](https://philippfrom.me/linting-playground/?variant=2&number=1)
+4. [Operate style](https://philippfrom.me/linting-playground/?variant=1)
+5. [Operate style with opacity](https://philippfrom.me/linting-playground/?variant=1&opacity=1)
+6. [Operate style with number](https://philippfrom.me/linting-playground/?variant=1&number=0)
 
 ![](docs/screenshot.png)
--- src/features/linting/LintingAnnotations.js
+++ src/features/linting/LintingAnnotations.js
@@ -24,8 +24,6 @@ export default class LintingAnnotations {
       reportsByElement = this._reportsByElement;
     }
 
-    console.log('removing overlay');
-
     this._overlays.remove({ type: 'linting' });
 
     Object.entries(reportsByElement).forEach(([ id, reports ]) => {
@@ -35,22 +33,25 @@ export default class LintingAnnotations {
         return;
       }
 
+      const isElementTypeNotAllowed = reports.length === 1
+        && reports[ 0 ].error
+        && reports[ 0 ].error.type === 'elementTypeNotAllowed';
+
       const overlay = domify(`
-        <div class="linting-annotation" title="Show Error">
-          <svg viewBox="0 0 24 24">
+        <div class="linting-annotation" title="${ isElementTypeNotAllowed ? 'Not supported' : 'Show Problem' }">
+          <svg viewBox="2 2 20 20">
             <path d="M12,5 C15.8659932,5 19,8.13400675 19,12 C19,15.8659932 15.8659932,19 12,19 C8.13400675,19 5,15.8659932 5,12 C5,8.13400675 8.13400675,5 12,5 Z M9.33333333,8 L8,9.33333333 L10.667,12 L8,14.6666667 L9.33333333,16 L12,13.333 L14.6666667,16 L16,14.6666667 L13.333,12 L16,9.33333333 L14.6666667,8 L12,10.666 L9.33333333,8 Z"></path>
           </svg>
-          <span>
-            ${ reports.length }
-          </span>
+          <span>${ reports.length }</span>
         </div>
       `);
 
-      overlay.addEventListener('click', () => {
-        this._eventBus.fire('refactorMe', { report: reports[ 0 ] });
-      });
+      if (!isElementTypeNotAllowed) {
+        overlay.addEventListener('click', () => {
+          this._eventBus.fire('refactorMe', { report: reports[ 0 ] });
+        });
+      }
 
-      console.log('adding overlay');
       const overlayId = this._overlays.add(element, 'linting', {
         position: this._getOverlayPosition(element),
         html: overlay,
@@ -70,6 +71,11 @@ export default class LintingAnnotations {
       return {
         bottom: 10,
         left: -10
+      };
+    case 2:
+      return {
+        bottom: -5,
+        left: 0
       };
     default:
       return {
